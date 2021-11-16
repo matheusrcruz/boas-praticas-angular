3e952666fa0df90fe73897f4b2248870763cc2a8
matheusrcruz <git.dev@outlook.com>
2021-11-15 21:38:47 -0300

    FIX: component Login render

--- front/gatitobook/src/app/app.module.ts
+++ front/gatitobook/src/app/app.module.ts
@@ -1,20 +1,14 @@
-import { HomeModule } from './home/home.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
